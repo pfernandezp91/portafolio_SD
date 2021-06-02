f92da61ad29a698c7180490ec0fedc49469d2315
OSCARIVAN2RAMOSGARCIA <oramos6@ucol.mx>
2021-06-02 13:04:59 -0500

    con mongo

--- mychat-oscar/index.js
+++ mychat-oscar/index.js
@@ -1,6 +1,16 @@
 var app=require("express")();
 var http=require("http").Server(app);
+const MongoClient = require('mongodb').MongoClient;
 var io=require("socket.io")(http);
+
+const uri = "mongodb+srv://oramos6:12345@cluster0.onkyo.mongodb.net/chat?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get("/",function(req,res){
    res.sendFile(__dirname+"/index.html"); 
@@ -19,6 +29,6 @@ io.on("connection",function(socket){
 });
 
 
-http.listen(3000,function(){
+http.listen(process.env.PORT||3000,function(){
     console.log("listen in 3000");
 });
