950e3e399bcc46f06d73f05db22d76c03beb7083
OSCARIVAN2RAMOSGARCIA <oramos6@ucol.mx>
2021-06-14 14:20:49 -0500

    hola

--- mychat-oscar/index.js
+++ mychat-oscar/index.js
@@ -2,11 +2,15 @@ require('dotenv/config');
 var app=require("express")();
 var http=require("http").Server(app);
 const MongoClient = require('mongodb').MongoClient;
+const bodyParser=require('body-parser');
 var io=require("socket.io")(http);
 const mongoose=require("mongoose");
 
-var User=require("./models/user")
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
+var User=require("./models/user")
+var Message=require("./models/message")
 mongoose.connect(process.env.DB_CONNECTION,
     {useNewUrlParser: true, useUnifiedTopology: true }
 ) .then(()=>console.log('base de datos conectada'))
@@ -15,6 +19,24 @@ mongoose.connect(process.env.DB_CONNECTION,
 app.get("/users",async(req,res)=>{
     var arrayUser=await User.find()
    res.jsonp(arrayUser); 
+});
+app.post("/message",async(req,res)=>{
+    try{
+        var messages= new Message();
+        messages.photo=req.body.photo;
+        messages.user=req.body.user;
+        messages.message=req.body.message;
+    
+    messages.save((error,commentStored)=>{
+        if(error)res.status(500).send({Mensaje: "hubo un error al guardar los datos"});
+        res.status(200).send({messages: commentStored});
+    });
+    
+}
+catch (error){
+    console.log(error);
+}
+   
 });
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/index.html")
---
+++ mychat-oscar/models/message.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema({
+  photo:String,
+    user:  String,
+  message: String
+});
+
+// Crear el modelo
+const Message = mongoose.model('Message', messageSchema);
+
+module.exports = Message;
